4a476edd6cbd303af139983b608f8e3947eff6d0
jniyfish <cry589036511@gmail.com>
2020-12-03 12:57:19 -0800

    part1 done

--- echo_server.cpp
+++ echo_server.cpp
@@ -25,138 +25,137 @@ typedef struct http_header_info
 
 }http_header_info;
 
-class Session{
+class Session : public std::enable_shared_from_this<Session>
+{
 	private:
 		ip::tcp::socket socket_;
+		ip::tcp::resolver resolver;
 		array<char, 4096> bytes_;
 		http_header_info header_info;
+		std::vector<char> in_buf_;
+		std::vector<char> out_buf_;
+		std::string remote_host_;
+		std::string remote_port_;
+		ip::tcp::socket out_socket_;
 
 	public:
-		Session(io_service &ioservice): socket_(ioservice){}
+		Session(io_service &ioservice): 
+		socket_(ioservice), 
+		resolver(ioservice), 
+		in_buf_(4096) ,
+		out_buf_(4096) ,
+		out_socket_(ioservice){}
 
 		ip::tcp::socket &socket(){
 			return socket_;
 		}
 		void start(){
 			auto h =  boost::bind(&Session::read_handler, this, _1, _2);
-			socket_.async_read_some(buffer(bytes_), h );
+			socket_.async_read_some(buffer(in_buf_), h );
 		}
-		void SetHttpEnvVar(){
-				string server_addr(header_info.host, 0, header_info.host.find(":"));
-				string server_port(header_info.host.begin() + header_info.host.find(":") + 1, header_info.host.end());
-				setenv("REQUEST_METHOD", header_info.method.c_str(), 1);
-				setenv("REQUEST_URI", header_info.uri.c_str(), 1);
-				setenv("QUERY_STRING", header_info.query.c_str(), 1);
-				setenv("SERVER_PROTOCOL", header_info.protocol.c_str(), 1);
-				setenv("HTTP_HOST", header_info.host.c_str(), 1);
-				setenv("SERVER_ADDR", server_addr.c_str(), 1);
-				setenv("SERVER_PORT", server_port.c_str(), 1);
-				setenv("REMOTE_ADDR", socket_.remote_endpoint().address().to_string().c_str(), 1);
-				setenv("REMOTE_PORT", to_string(socket_.remote_endpoint().port()).c_str(), 1);
-		}
-		void parse_header(){
-			char *parseFirst=NULL;
-			char *parseSec=NULL;
-			//GET /panel.cgi HTTP/1.1
-			//Host: nplinux1.cs.nctu.edu.tw:7001
-
-			parseFirst = strtok(bytes_.data(), "\r\n");
-			parseSec =  strtok(NULL, "\r\n");
-			parseFirst = strtok(parseFirst, " \r\n");
-
-			int q=0;
-			while (q!=3){
-				header_info.method = (q==0) ? parseFirst : header_info.method  ;
-				header_info.path = (q==1) ? parseFirst : header_info.path ;
-				header_info.uri = (q==1) ? parseFirst : header_info.uri ;
-				header_info.protocol  = (q==2) ?  parseFirst : header_info.protocol;
-				parseFirst = strtok(NULL, " \r\n");
-				q++;
-			}	
-			parseSec = strtok(parseSec, " \r\n");
-			parseSec =  strtok(NULL, "\r\n");
-			header_info.host = parseSec;
-
-			size_t loc = header_info.path.find("?") + 1;  //find ? position
-			if (loc-1 != string::npos){
-				header_info.query.assign(header_info.path, loc, header_info.path.size() - loc);
-				header_info.path.erase(header_info.path.find("?"), string::npos);   //remove all after ?
-			}
-			else{
-				header_info.query.assign("");
-			}
-		}
-		void runCgi(){
-			pid_t PID;
-			int fd = socket_.lowest_layer().native_handle();
-			string complete_path = ".";
-			signal(SIGCHLD, Session::childHandler);
-			PID = fork();
-			if(PID == 0){
-				dup2(fd, STDOUT_FILENO);
-				close(fd);
-				complete_path += header_info.path;
-			/*	if (header_info.path.compare("/panel.cgi") == 0){
-					if (execlp(complete_path.c_str(), "panel.cgi", NULL) == -1){
-						cerr << "Exec error!" << endl;
-						exit(2);
-					}   
-				}
-				else if (header_info.path.compare(0, 12, "/console.cgi") == 0){
-					if (execlp(complete_path.c_str(), "console.cgi", NULL) == -1){
-						cerr << "Exec error!" << endl;
-						exit(2);
-					}  
-				}
-				else if((header_info.path.compare(0, 13, "/printenv.cgi") == 0)){
-					if (execlp(complete_path.c_str(), "printenv.cgi", NULL) == -1){
-						cerr << "Exec error!" << endl;
-						exit(2);
-					}  
-				}
-				else if((header_info.path.compare(0, 10, "/hello.cgi") == 0)){
-					if (execlp(complete_path.c_str(), "hello.cgi", NULL) == -1){
-						cerr << "Exec error!" << endl;
-						exit(2);
-					}  
-				}
-				else if((header_info.path.compare(0, 12, "/welcome.cgi") == 0)){
-					if (execlp(complete_path.c_str(), "welcome.cgi", NULL) == -1){
-						cerr << "Exec error!" << endl;
-						exit(2);
-					}  
-				}*/
-				if (header_info.path.find(".cgi") != string::npos){
-                    header_info.path.erase(header_info.path.begin());
-                    if (execlp(complete_path.c_str(), header_info.path.c_str(), NULL) == -1){
-                        cerr << "Exec error!" << endl;
-                        exit(2);
-                    }
-                }
-			}
-			else if(PID==-1){
-				cerr << "Fork error!" << endl;
-				exit(EXIT_FAILURE);
-			}
-			else
-				socket_.close();
-		}
+	
 		void read_handler(const boost::system::error_code &ec, size_t bytes_transferred){
 			if (!ec){
-				string reply("HTTP/1.1 200 OK\r\n");
-				boost::system::error_code ec;
-				socket_.write_some(buffer(reply), ec);
-
-				Session::parse_header();
-				Session::SetHttpEnvVar();
-				Session::runCgi();
+				if ( in_buf_[0] == 0x04){
+					cout << "SOCK4" << endl;
+					if(in_buf_[1] == 0x01){
+						cout << "connect request" << endl;
+						remote_host_ = boost::asio::ip::address_v4(ntohl(*((uint32_t*)&in_buf_[4]))).to_string();
+						remote_port_ = std::to_string(ntohs(*((uint16_t*)&in_buf_[2])));
+						cout << remote_host_ <<":"<<remote_port_ <<endl;
+						do_resolve();
+						
+					}
+				}
 			}
 		}
-		static void childHandler(int signo){
-			int status;
-			while (waitpid(-1, &status, WNOHANG) > 0)	{;}
+		void do_resolve(){
+			//auto self(shared_from_this());
+			ip::tcp::resolver::query query(remote_host_, remote_port_);
+			resolver.async_resolve(query,
+			[this](const boost::system::error_code& ec, ip::tcp::resolver::iterator it)
+			{
+				if (!ec)
+				{
+					cout << "a" << endl;
+					do_connect(it);
+				}
+			});
 		}
+		void do_connect(ip::tcp::resolver::iterator& it){
+			out_socket_.async_connect(*it, 
+			[this](const boost::system::error_code& ec)
+			{
+				if (!ec)
+				{
+					cout << "b" <<endl;
+					cout << "Reply" << endl;
+						in_buf_[0] = 0x00; in_buf_[1] = 0x5A;
+						in_buf_[2] = 0x00; in_buf_[3] = 0x00; 
+						in_buf_[4] = 0x00; in_buf_[5] = 0x00; 
+						in_buf_[6] = 0x00; in_buf_[7] = 0x00; 
+						boost::asio::async_write(socket_, boost::asio::buffer(in_buf_, 8), // Always 10-byte according to RFC1928
+						[this](boost::system::error_code ec, std::size_t length)
+						{
+							if (!ec)
+							{}
+						});
+					do_read(3);
+				}
+			});
+		}
+		void do_read(int direction){
+			size_t length = 4096;
+			// We must divide reads by direction to not permit second read call on the same socket.
+			if (direction & 0x1)
+				socket_.async_receive(boost::asio::buffer(in_buf_),
+					[this](boost::system::error_code ec, std::size_t length)
+					{
+						if (!ec)
+						{
+							std::ostringstream what; what << "--> " << std::to_string(length) << " bytes";
 
+							do_write(1, length);
+						}
+
+					});
+
+			if (direction & 0x2)
+				out_socket_.async_receive(boost::asio::buffer(out_buf_),
+					[this](boost::system::error_code ec, std::size_t length)
+					{
+						if (!ec)
+						{
+							std::ostringstream what; what << "<-- " << std::to_string(length) << " bytes";
+
+							do_write(2, length);
+						} 
+					});
+		}
+		void do_write(int direction, std::size_t Length)
+			{		
+				switch (direction)
+				{
+				case 1:
+					boost::asio::async_write(out_socket_, boost::asio::buffer(in_buf_, Length),
+						[this, direction](boost::system::error_code ec, std::size_t length)
+						{
+							if (!ec)
+								do_read(direction);
+
+						});
+					break;
+				case 2:
+					boost::asio::async_write(socket_, boost::asio::buffer(out_buf_, Length),
+						[this, direction](boost::system::error_code ec, std::size_t length)
+						{
+							if (!ec)
+								do_read(direction);
+
+						});
+					break;
+				}
+			}
 };
 
 class server{
